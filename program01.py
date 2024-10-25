863e4a77265aca9f8a75e22ca4f4a12cf6c464e2
UnlimitedBl00dWorks <MarquiseDouglas2@gmail.com>
2024-10-25 09:25:09 -0500

    Added a random_food function

--- program01.py
+++ program01.py
@@ -2,6 +2,14 @@
 import random
 def random_number():
     number_gen = random.randint(1,20)
-    print(number_gen)
+    print(f"You have ${number_gen}")
 
 random_number()
+
+def random_food():
+    food_list = ["an apple", "pie", "soup", "a danish"]
+    random_index = random.randint(0,3)
+    list_selection = food_list[random_index]
+    print(f"I'm in the mood for {list_selection}")
+
+random_food()
